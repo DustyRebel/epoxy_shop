9e3c286119b4ea6af7f57d4c943d113e8259bbbc
DustyRebel <202270441+DustyRebel@users.noreply.github.com>
2025-05-09 03:08:53 +0700

    item deletion button removed from admin page

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -11,7 +11,6 @@ import {
 import CreateType from "../components/modals/CreateType";
 import CreateColor from "../components/modals/CreateColor";
 import CreateItem from "../components/modals/CreateItem";
-import DeleteItem from "../components/modals/DeleteItem";
 import CreateGItem from "../components/modals/CreateGItem";
 import GDeleteItem from "../components/modals/GDeleteItem";
 import CreateShipping from "../components/modals/CreateShipping";
@@ -27,7 +26,6 @@ const Admin = () => {
   const [typeVisible, setTypeVisible] = useState(false);
   const [itemVisible, setItemVisible] = useState(false);
   const [gitemVisible, setGItemVisible] = useState(false);
-  const [itemDeleteVisible, setItemDeleteVisible] = useState(false);
   const [gitemDeleteVisible, setGItemDeleteVisible] = useState(false);
   const [shippingVisible, setShippingVisible] = useState(false);
 
@@ -51,6 +49,10 @@ const Admin = () => {
     loadData();
   }, []);
 
+  //import DeleteItem from "../components/modals/DeleteItem";
+  //const [itemDeleteVisible, setItemDeleteVisible] = useState(false);
+  //<Button variant="outline-dark" className="mt-2 p-2" onClick={() => setItemDeleteVisible(true)}>Удалить товар</Button>
+  //<DeleteItem show={itemDeleteVisible} onHide={() => setItemDeleteVisible(false)} />
   return (
     <Container className="d-flex flex-column">
 
@@ -58,7 +60,6 @@ const Admin = () => {
       <Button variant="outline-dark" className="mt-2 p-2" onClick={() => setTypeVisible(true)}>Добавить тип</Button>
       <Button variant="outline-dark" className="mt-2 p-2" onClick={() => setColorVisible(true)}>Добавить цвет</Button>
       <Button variant="outline-dark" className="mt-2 p-2" onClick={() => setItemVisible(true)}>Добавить товар</Button>
-      <Button variant="outline-dark" className="mt-2 p-2" onClick={() => setItemDeleteVisible(true)}>Удалить товар</Button>
       <Button variant="outline-dark" className="mt-2 p-2" onClick={() => setGItemVisible(true)}>Добавить предмет в галерею</Button>
       <Button variant="outline-dark" className="mt-2 p-2" onClick={() => setGItemDeleteVisible(true)}>Удалить предмет галереи</Button>
       <Button variant="outline-dark" className="mt-2 p-2" onClick={() => setShippingVisible(true)}>Добавить способ доставки</Button>
@@ -75,7 +76,6 @@ const Admin = () => {
       <CreateType show={typeVisible} onHide={() => setTypeVisible(false)} />
       <CreateColor show={colorVisible} onHide={() => setColorVisible(false)} />
       <CreateItem show={itemVisible} onHide={() => setItemVisible(false)} />
-      <DeleteItem show={itemDeleteVisible} onHide={() => setItemDeleteVisible(false)} />
       <CreateGItem show={gitemVisible} onHide={() => setGItemVisible(false)} />
       <GDeleteItem show={gitemDeleteVisible} onHide={() => setGItemDeleteVisible(false)} />
       <CreateShipping show={shippingVisible} onHide={() => setShippingVisible(false)} />
